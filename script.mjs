e6c03db8edf859d2d9e1b703e961060a1c3c8f92
Sergey Lyapustin <s.lyapustin@gmail.com>
2023-01-26 15:16:23 +0100

    Initial pre-commit hook with hardcoded Snowflake language.

---
+++ script.mjs
@@ -0,0 +1,32 @@
+import { format } from 'sql-formatter';
+import fs from "fs";
+
+
+if (process.argv.length === 2) {
+  console.error('Expected at least one argument!');
+  process.exit(1);
+}
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  };
+
+  const formatted = format(data, {
+    language: 'snowflake',
+    tabWidth: 2,
+    keywordCase: 'upper',
+    linesBetweenQueries: 2,
+  });
+
+  if (data != formatted) {
+    console.log(`${filename} was reformatted.`)
+    fs.writeFile(filename, formatted, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
